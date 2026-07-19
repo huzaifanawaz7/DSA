51c3a11757a19230031b2d2b54e10e0fe10b4538
huzaifa <huzaifanawaz407@gmail.com>
2026-07-19 11:04:07 +0500

    pattern

--- triangle.cpp
+++ triangle.cpp
@@ -2,10 +2,53 @@
 using namespace std;
 int main(){
     int n=4;
-    for (int i=0 ; i<n;i++){
-        for (int j=0 ;j<i+1;j++){
-            cout<<"*";
-        }
-        cout<<endl;
+    // for (int i=0 ; i<n;i++){
+    //     for (int j=0 ;j<i+1;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+//  for (int i=0 ; i< n ;i++){
+//     int num=1;
+
+//           for (int j=0;j<=i;j++){
+//             cout<<num;
+//             num++;
+
+//           }
+
+//           cout<<endl;
+//  }
+
+// inverted triangle
+// char a='A';
+// for(int i=0; i < n;i++){
+//     for ( int j=0;j<i;j++){
+//       cout<<" ";
+//     }
+//     for (int j=0;j<n-i;j++){
+//         cout<<a;
+//     }
+//     a++;
+//    cout<<endl;
+
+
+
+// }}
+for (int i=0;i<n;i++){
+    for (int j=0;j<n-i-1;j++){
+        cout<<" ";
     }
+for (int j=1;j<=i+1;j++){
+    cout<<j;
+
+}
+for (int j=i;j>0;j--){
+     cout<<j;   
+}
+}
+cout<<endl;
+
 }
