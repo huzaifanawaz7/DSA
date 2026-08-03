163216a6de13635f5cf261bed8c74679ad1d7c8a
huzaifa <huzaifanawaz407@gmail.com>
2026-08-03 11:54:43 +0500

    alternate swap

---
+++ alternateswap.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void swaparray(int arr[],int n){
+    for( int i=0; i<n ; i=i+2){
+        if(i+1<n){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+int arr[6]={1,2,3,4,5,6};// even array
+ swaparray(arr,6);
+ 
+}
