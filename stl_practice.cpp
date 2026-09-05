a238cd1f64e106a3e07442dac0f8290c36b10ce4
huzaifa <huzaifanawaz407@gmail.com>
2026-09-05 10:16:45 +0500

    some change

--- stl_practice.cpp
+++ stl_practice.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+#include<queue>
 using namespace std;
 int main(){
 // vector<int>v;
@@ -12,13 +13,13 @@ int main(){
 // v.push_back(311);
 // cout<<"size"<<v.size();
 // cout<<"capacity"<<v.capacity();
-stack<string>s;
+queue<string>s;
 s.push("ali");
 s.push("lal");
 s.push("kashif");
 s.push("kalu");
-cout<<"the top element is"<<s.top();
+cout<<"the top element is"<<s.front();
 s.pop();
-cout<<"the top element is"<<s.top();
+cout<<"the top element is"<<s.front();
 cout<<"the size of the stack"<<s.size()<<endl;
 }
